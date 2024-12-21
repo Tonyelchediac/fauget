abb361e13ef76bc6248986c5f0d0b34932d0ac03
Tonyelchediac <130777962+Tonyelchediac@users.noreply.github.com>
2024-12-21 14:54:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('.shop-name', { delay: 200, distance: '50px', origin: 'top' });
+ScrollReveal().reveal('.home p', { delay: 400, distance: '30px', origin: 'bottom' });
+ScrollReveal().reveal('.scroll-indicator', { delay: 600, distance: '20px', origin: 'bottom' });
+ScrollReveal().reveal('.social-icons', { delay: 200, distance: '30px', origin: 'bottom' });
+ScrollReveal().reveal('.section-title', { delay: 200, distance: '30px', origin: 'top' });
+ScrollReveal().reveal('.item', { 
+    delay: 200, 
+    distance: '30px', 
+    origin: 'bottom',
+    interval: 200 
+});
